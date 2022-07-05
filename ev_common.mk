3b0dd19e92f0c4b663ae4db7b833584d300d833c
William Bellavance <flintman@flintmancomputers.com>
2022-07-05 05:09:54 -0400

    bring on compatibility matrix
    Change-Id: I0bcfd71a70645b4bf984d10f1a3ff8d0a6cd06b7

--- ev_common.mk
+++ ev_common.mk
@@ -8,3 +8,6 @@ DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-ev
 
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
+PRODUCT_PACKAGES += \
+    evervolv_compatibility_matrix.xml
